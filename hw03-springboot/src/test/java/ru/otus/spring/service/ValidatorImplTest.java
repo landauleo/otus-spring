d850044266578158f935d1a80080c512573be6c2
ruanaaf <Anna.G@RAIFFEISEN.ru>
2022-10-25 14:51:07 +0300

    hw03

--- hw03-springboot/src/test/java/ru/otus/spring/service/ValidatorImplTest.java
+++ hw03-springboot/src/test/java/ru/otus/spring/service/ValidatorImplTest.java
@@ -1,11 +1,6 @@
 package ru.otus.spring.service;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.Collection;
 import java.util.List;
-import java.util.Locale;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -14,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.otus.spring.MainCommandLineRunner;
-import ru.otus.spring.domain.Option;
-import ru.otus.spring.domain.Question;
 import ru.otus.spring.repository.QuestionRepository;
 import ru.otus.spring.service.validator.Validator;
 
@@ -25,7 +18,7 @@ class ValidatorImplTest {
     @Autowired
     private Validator validator;
 
-    @Autowired
+    @MockBean
     private QuestionRepository questionRepository;
 
     @MockBean
@@ -34,11 +27,7 @@ class ValidatorImplTest {
     @Test
     @DisplayName("Не бросает ошибку при вводе неправильных ответов")
     void testValidateWrongAnswers() {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(baos);
-        System.setOut(ps);
-
-        List<String> rightAnswers = questionRepository.read().stream().map(Question::getOptions).flatMap(Collection::stream).filter(Option::isRight).map(Option::getText).collect(Collectors.toList());
+        List<String> rightAnswers = List.of("answer1", "answer2", "answer3", "answer4");
         List<String> wrongAnswers = List.of("blah-blah", "blah-blah", "blah-blah", "blah-blah");
 
         Assertions.assertDoesNotThrow(() -> validator.validate(rightAnswers, wrongAnswers));
@@ -48,11 +37,7 @@ class ValidatorImplTest {
     @Test
     @DisplayName("Не бросает ошибку при вводе правильных ответов в правильной локали")
     void testValidateRightInRightLocaleAnswers() {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(baos);
-        System.setOut(ps);
-
-        List<String> rightAnswers = questionRepository.read().stream().map(Question::getOptions).flatMap(Collection::stream).filter(Option::isRight).map(option -> option.getText().toLowerCase(Locale.ROOT)).collect(Collectors.toList());
+        List<String> rightAnswers = List.of("1976", "5", "польша", "зелёный", "фиона");
         List<String> actualAnswers = List.of("1976", "5", "польша", "зелёный", "фиона");
 
         Assertions.assertDoesNotThrow(() -> validator.validate(rightAnswers, actualAnswers));
@@ -61,11 +46,7 @@ class ValidatorImplTest {
     @Test
     @DisplayName("Не бросает ошибку при вводе правильных ответов в неправильной локали")
     void testValidateRightInWrongLocaleAnswers() {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(baos);
-        System.setOut(ps);
-
-        List<String> rightAnswers = questionRepository.read().stream().map(Question::getOptions).flatMap(Collection::stream).filter(Option::isRight).map(Option::getText).collect(Collectors.toList());
+        List<String> rightAnswers = List.of("1976", "5", "польша", "зелёный", "фиона");
         List<String> actualAnswers = List.of("1976", "5", "poland", "green", "fIoNa");
 
         Assertions.assertDoesNotThrow(() -> validator.validate(rightAnswers, actualAnswers));
