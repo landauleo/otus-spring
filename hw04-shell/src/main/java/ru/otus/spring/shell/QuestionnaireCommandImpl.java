5773ea64af747916200266a306ccc89b0e18516c
ruanaaf <Anna.G@RAIFFEISEN.ru>
2022-11-08 23:52:51 +0300

    hw04

--- hw04-shell/src/main/java/ru/otus/spring/shell/QuestionnaireCommandImpl.java
+++ hw04-shell/src/main/java/ru/otus/spring/shell/QuestionnaireCommandImpl.java
@@ -1,5 +1,7 @@
 package ru.otus.spring.shell;
 
+import java.util.Locale;
+
 import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
@@ -9,37 +11,36 @@ import ru.otus.spring.service.IOService;
 import ru.otus.spring.service.MessageSourceService;
 import ru.otus.spring.service.QuestionnaireService;
 
-import java.util.Locale;
-import java.util.concurrent.atomic.AtomicReference;
-
 import static org.springframework.shell.Availability.available;
 import static org.springframework.shell.Availability.unavailable;
 
 @ShellCommandGroup("Questionnaire auth commands")
 @ShellComponent
-public class AuthenticationService {
+public class QuestionnaireCommandImpl implements QuestionnaireCommand {
 
     private final MessageSourceService messageSource;
     private final IOService ioService;
     private final QuestionnaireService questionnaireService;
     private String userName;
 
-    public AuthenticationService(MessageSourceService messageSource, IOService ioService, QuestionnaireService questionnaireService) {
+    public QuestionnaireCommandImpl(MessageSourceService messageSource, IOService ioService, QuestionnaireService questionnaireService) {
         this.messageSource = messageSource;
         this.ioService = ioService;
         this.questionnaireService = questionnaireService;
     }
 
+    @Override
     @ShellMethod(value = "Authenticate user", key = {"1", "start", "auth", "authenticate"})
     @ShellMethodAvailability(value = "shouldAuthenticate")
     public void authenticate() {
         messageSource.printMessage("authentication.user", null);
         userName = ioService.readString().trim().toLowerCase(Locale.ROOT);
-        messageSource.printMessage("greeting.user", new String[]{String.valueOf(userName)});
+        messageSource.printMessage("greeting.user", new String[]{userName});
         questionnaireService.run();
     }
 
     private Availability shouldAuthenticate() {
         return userName == null ? available() : unavailable("User has already introduced");
     }
+
 }
