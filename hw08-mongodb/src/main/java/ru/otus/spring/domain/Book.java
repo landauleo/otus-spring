203e2df331cf6f3d8ddf94234cf7ad1aaea408b2
ruanaaf <Anna.G.NOSOVA@raiffeisen.ru>
2023-04-05 06:36:09 +0300

    hw08

--- hw08-mongodb/src/main/java/ru/otus/spring/domain/Book.java
+++ hw08-mongodb/src/main/java/ru/otus/spring/domain/Book.java
@@ -1,20 +1,16 @@
 package ru.otus.spring.domain;
 
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Getter
-@Setter
-@ToString(exclude = {"genre", "author"}) //чтобы такого не делать нужно юзать ДТО, но это уже другая история
+@Data
 @NoArgsConstructor
-@EqualsAndHashCode
+@AllArgsConstructor
 @Document(collection = "book")
 public class Book {
 
@@ -26,13 +22,6 @@ public class Book {
     private Genre genre;
 
     private Author author;
-
-    public Book(ObjectId id, String name, Genre genre, Author author) {
-        this.id = id;
-        this.name = name;
-        this.genre = genre;
-        this.author = author;
-    }
 
     public Book(String name, Genre genre, Author author) {
         this.name = name;
