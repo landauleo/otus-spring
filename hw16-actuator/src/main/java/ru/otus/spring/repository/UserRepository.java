ebb0a96fd4fa53692e3ea2a57d2409e6233438ea
ruanaaf <Anna.G.NOSOVA@raiffeisen.ru>
2023-09-09 02:20:33 +0300

    hw16

---
+++ hw16-actuator/src/main/java/ru/otus/spring/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package ru.otus.spring.repository;
+
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import ru.otus.spring.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+
+    Optional<User> findByUsername(String username);
+
+}
