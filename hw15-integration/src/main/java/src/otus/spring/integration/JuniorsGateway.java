1661992d9bc8541b66157d9b8ce053acec5a8f80
ruanaaf <Anna.G.NOSOVA@raiffeisen.ru>
2023-09-12 23:47:33 +0300

    hw15

--- hw15-integration/src/main/java/src/otus/spring/integration/JuniorsGateway.java
+++ hw15-integration/src/main/java/src/otus/spring/integration/JuniorsGateway.java
@@ -6,12 +6,11 @@ import java.util.Collection;
 import org.springframework.integration.annotation.Gateway;
 import org.springframework.integration.annotation.MessagingGateway;
 import src.otus.spring.domain.Junior;
-import src.otus.spring.domain.Middle;
 
 @MessagingGateway
 public interface JuniorsGateway {
 
-    @Gateway(requestChannel = "juniorsChannel", replyChannel = "middlesChannel")
-    Collection<Middle> process(Collection<Junior> juniors);
+    @Gateway(requestChannel = "juniorsChannel")
+    void process(Collection<Junior> juniors);
 
 }
